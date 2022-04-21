f24f397bad0f0fb9ae9471e355b8f8ba7efc73db
Brisa-Codes <brisamukunde1@gmail.com>
2022-04-21 23:49:05 +0300

    hardhat contract

---
+++ hardhat-tut/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x54201399A9d3b2148bD0640ab9AB6fe7d60650B9";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
